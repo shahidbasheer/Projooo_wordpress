49c76421bca51e3f6a362068e24060f5fdbf0279
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-02 22:25:09 +0500

    new page

--- includes/list-categories-widget.php
+++ includes/list-categories-widget.php
@@ -40,7 +40,7 @@ class list_categories_widget extends WP_Widget {
 
 							
 								<?php foreach($cats as $cat) { ?>
-										<?php  get_term_link($cat->slug, $taxonomy); die();  ?>
+										<?php  get_term_link($cat->slug, $taxonomy);  ?>
 										<li><a href="<?php echo get_term_link($cat->slug, $taxonomy); ?>" title="<?php sprintf( __( "View all posts in %s" ), $cat->name ); ?>"><?php echo $cat->name ?> (<span class="count"><?php echo $cat->category_count; ?></span>)</a></li>								
 		
 								<?php } ?>
