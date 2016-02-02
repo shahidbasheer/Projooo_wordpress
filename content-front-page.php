6b8d3c4ecadfd9eebb5bc1946452dac18d7411fa
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-03 00:03:24 +0500

    new page

--- content-front-page.php
+++ content-front-page.php
@@ -11,139 +11,16 @@
 		
      	
 
-     	   <section class="container-projoo intro">
-     	   	 <p>Projoo is an online project management app that helps you plan your work and keep track of your progress. It also lets the people in the project communicate easily, discuss ideas, and stay updated. This lets you deliver quality results on time.</p>
-     	   	 <img src="<?php echo  theme_root . '/'; ?>includes/img/dummey-pic.png" class="img-responsive center-block">
-     	   </section>
-
-     	   <div class="devider-100"></div>
-
-     	   <section class="container-projoo anotherintro m-b-80">
-     	   	<h2 class="blue-text bold">Start managing your business more effectively.</h2>
-     	   	<p>Empower your team to make an impact in dynamic environments. Project management is central to innovation. The best project management software helps teams embrace change and turn plans and ideas into reality.</p>
-     	   </section>
-
-     	   <section class="container-projoo portfoli">
-     	   	<div class="row">
-     	   		<div class="col-xs-12 col-sm-4 col-md-4">
-					<img class="" src="<?php echo  theme_root . '/'; ?>includes/img/icon-1.png" width="140" alt="">
-					<h4>Project Card</h4>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<img class="" src="<?php echo  theme_root . '/'; ?>includes/img/icon-2.png" width="140" alt="">
-					<h4>Team Discussion</h4>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<img class="" src="<?php echo  theme_root . '/'; ?>includes/img/icon-2.png" width="140" alt="">
-					<h4>To-Do List</h4>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
-				</div>
-     	   	</div>
-     	   </section>
-
-
-     	   <section class="less-confision">
-	     	   	<div class="container-projoo">
-	     	   		<div class="row">
-	     	   			<div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-	     	   				<h2 class='m-b-30'><span class="blue-text">Less Confusion</span>
-	     	   					<br>
-	     	   				happy people <br>productive people</h2>
-
-	     	   				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-	     	   			</div>
-	     	   			<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
-	     	   				<ul class="list-unstyled">
-	     	   					
-	     	   					<li><i class="glyphicon glyphicon-check"></i>More Team Communication</li>
-	     	   					<li><i class="glyphicon glyphicon-check"></i>Trust & Appreciation from Clients</li>
-	     	   					<li><i class="glyphicon glyphicon-check"></i>Client Involvement</li>
-	     	   					<li><i class="glyphicon glyphicon-check"></i>Keep everyone on the same page & your projects in cruise-control.</li>
-	     	   					<li><i class="glyphicon glyphicon-check"></i>Less Meetings</li>
-	     	   					<li><i class="glyphicon glyphicon-check"></i>Misunderstandings </li>
-	     	   					<li><i class="glyphicon glyphicon-check"></i>Painful email thread</li>
-	     	   					
-	     	   				</ul>
-	     	   			</div>
-	     	   		</div>
-	     	   	</div>
-     	   </section>
-
-
-     	    <section class="container-projoo portfoli m-b-60">
-	     	   	<div class="row">
-	     	   		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-						<img class="img-circle" src="<?php echo  theme_root . '/'; ?>includes/img/icon-1.png" width="140" alt="">
-						<h4>Michael Robson</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
-					</div>
-					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-						<img class="img-circle" src="<?php echo  theme_root . '/'; ?>includes/img/icon-1.png" width="140" alt="">
-						<h4>Michael Robson</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
-					</div>
-					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-						<img class="img-circle" src="<?php echo  theme_root . '/'; ?>includes/img/icon-1.png" width="140" alt="">
-						<h4>Michael Robson</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
-					</div>
-	     	   	</div>
-     	   </section>
-
-     	   <section class="cta">
-	     	   	<div class="container-projoo">
-	     	   		<h2 class="m-b-50 center-block"><span class="blue-text">Projoo</span> is for small medium size business team members. Everything Right Where You Need It</h2>
-	     	   		<div class="row">
-	     	   			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 m-b-20">
-	     	   				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quasi libero iure omnis nisi voluptas adipisci fugiat cupiditate vitae sed.</p>
-	     	   			</div>
-	     	   			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 m-b-20">
-	     	   				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quasi libero iure omnis nisi voluptas adipisci fugiat cupiditate vitae sed.</p>
-	     	   			</div>
-	     	   			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 m-b-20">
-	     	   				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quasi libero iure omnis nisi voluptas adipisci fugiat cupiditate vitae sed.</p>
-	     	   			</div>
-	     	   		</div> <!-- row -->
-	     	   		<div class="row">
-	     	   			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 m-b-20">
-	     	   				<p class="p-t-b">Anyone that is scheduling a project will benefit from being able to do this online. Any company or individual that is planning either an in house project or a project for a customer can benefit greatly from being able to schedule the project in a gantt view and share it with others. Some examples include.</p>
-	     	   			</div>
-	     	   		</div> <!-- row -->
-	     	   		<div class="row">
-	     	   			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-	     	   				<ul class="list-unstyled">
-	     	   					<li><a href="">Creative/Design Agencies</a></li>
-	     	   					<li><a href="">Event Planning</a></li>
-	     	   					<li><a href="">Software Developers</a></li>
-	     	   					<li><a href="">Construction Companies</a></li>
-	     	   					<li><a href="">Universities</a></li>
-	     	   					<li><a href="">Manufacturing</a></li>
-	     	   					<li><a href="">Teams from Fortune 500 companies</a></li>
-	     	   					<li><a href="">Any business that wants to plan and communicate better on their projects.</a></li>
-	     	   				</ul>
-	     	   			</div>
-	     	   			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-	     	   				<div class="cta-price">
-	     	   					<h3>simple price</h3>
-	     	   					<span class="blue-text price">11.99</span>
-	     	   					<span class="super">per month</span>
-	     	   					<p class='no-margin-b'>unlimited projects/ work Board</p>
-	     	   					<p>Unlimited Users</p>
-	     	   					<i class='glyphicon glyphicon-pushpin'></i>
-	     	   				</div>
-	     	   			</div>
-	     	   		</div>
-	     	   	</div> 
-     	   </section> <!-- cta -->
-
-     	   <section class="container-projoo cta-2">
-     	   	<h2 class="center-block m-b-30">Try Projoo -is it time to five your project the love they deserve?</h2>
-			<img src="<?php echo  theme_root . '/'; ?>includes/img/arrow-left-bottom.png" class="arrow hidden-xs">
-			<button type="button" class="btn btn-green">Request for Early Access</button>
-     	   	
-     	   </section>
+		<?php 
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post(); 
+				//
+				the_content();
+				//
+			} // end while
+		} // end if
+		?>
 
      	  
 
