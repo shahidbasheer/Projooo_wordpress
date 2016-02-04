2d2b12f9b825fa19f25c59370f5d2e4d08baf917
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-05 03:16:48 +0500

    about us page

--- content-about-us-page.php
+++ content-about-us-page.php
@@ -12,74 +12,61 @@
 						
 					
 
-			     	   <section class="container-projoo anotherintro">
-			     	   	<h2 class="m-b-10">Projooo and Business Value.</h2>
-			     	   	<!-- <img src="<?php// echo  theme_root . '/'; ?>includes/img/social-icon.png"> -->
-			     	   	 <p>We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.</p>
-			     	   </section>
+			     	        	   <section class="container-projoo anotherintro">
+			     	        	   	<h2 class="m-b-10">Projooo and Business Value.</h2>
+			     	        	   	<img src="<?php echo  theme_root . '/'; ?>includes/img/social-icon.png" class="m-b-20">
+			     	        	   	 <p>We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.</p>
+			     	        	   </section>
 
-			     	   <section class="container-projoo anotherintro m-b-80">
-			     	   	<h2 class="m-b-40">We are build software for Small Business.</h2>
-			     	   	<p>We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.</p>
-			     	   </section>
+			     	        	   <section class="container-projoo anotherintro m-b-80">
+			     	        	   	<h2 class="m-b-40">We are build software for Small Business.</h2>
+			     	        	   	<p>We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.We’re dedicated to selling innovative and creative products to designers at affordable prices. We also help designers maximize their revenue by actively promoting the products that they sell through our market.</p>
+			     	        	   </section>
 
 
 
-			     	   <section class="container-projoo team m-b-80">
-			     	   	<h2 class="m-b-40">We love our work and our team.</h2>
-			     	   	<ul class="list-inline">
-			     	   		<li>
-			     	   			<a href="">
-			     	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/team-1.png">
-			     	   				<br>
-			     	   				<span class="name">Blake</span>
-			     	   			</a>
-			     	   			
-			     	   		</li>
-			     	   		<li>
-			     	   			<a href="">
-			     	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/team-1.png">
-			     	   				<br>
-			     	   				<span class="name">Thomas</span>
-			     	   			</a>
-			     	   			
-			     	   		</li>
-			     	   		<li>
-			     	   			<a href="">
-			     	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/team-1.png">
-			     	   				<br>
-			     	   				<span class="name">David</span>
-			     	   			</a>
-			     	   			
-			     	   		</li>
-			     	   		<li>
-			     	   			<a href="">
-			     	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/team-1.png">
-			     	   				<br>
-			     	   				<span class="name">Rohit</span>
-			     	   			</a>
-			     	   			
-			     	   		</li>
-			     	   		<li>
-			     	   			<a href="">
-			     	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/team-1.png">
-			     	   				<br>
-			     	   				<span class="name">Blake</span>
-			     	   			</a>
-			     	   			
-			     	   		</li>
+			     	        	   <section class="container-projoo team m-b-80">
+			     	        	   	<h2 class="m-b-40">We love our work and our team.</h2>
+			     	        	   	<ul class="list-inline">
+			     	        	   		<li>
+			     	        	   			<a href="">
+			     	        	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/anil-nair.png">
+			     	        	   				<br>
+			     	                                 <h4>Anil Nair</h4>
+			     	        	   				<span class="name">Product Manager</span>
+			     	        	   			</a>
+			     	        	   			
+			     	        	   		</li>
+			     	        	   		<li>
+			     	        	   			<a href="">
+			     	        	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/amy-monaco.png">
+			     	        	   				<br>
+			     	                                 <h4>Amy M. Monaco</h4>
+			     	        	   				<span class="name">Digital Marketing & Content Manager</span>
+			     	        	   			</a>
+			     	        	   			
+			     	        	   		</li>
+			     	        	   		<li>
+			     	        	   			<a href="">
+			     	        	   				<img src="<?php echo  theme_root . '/'; ?>includes/img/timothy-paige.png">
+			     	        	   				<br>
+			     	                                 <h4>Timothy R. Paige</h4>
+			     	        	   				<span class="name">Product Manager</span>
+			     	        	   			</a>
+			     	        	   			
+			     	        	   		</li>    	   		
 
-			     	   	</ul>
-			     	   </section>
+			     	        	   	</ul>
+			     	        	   </section>
 
-			     	  
+			     	        	  
 
-			     	   <section class="container-projoo cta-2">
-	     	       	   	<h2 class="center-block m-b-30">Try Proojo – Maximum visibility with minimum clutter of tasks.</h2>
-	     	  			<img src="http://www.proojo.com/wp-content/themes/projoo_theme/includes/img/arrow-left-bottom.png" class="arrow hidden-xs">
-	     	  			<button type="button" class="btn btn-green" data-toggle="modal" data-target="#modal-form">Request for Early Access</button>
-	     	       	   	
-	     	       	   </section>
+			     	        	   <section class="container-projoo cta-2">
+			     	        	   	<h2 class="center-block m-b-30">Try Projoo -is it time to five your project the love they deserve?</h2>
+			     	   			<img src="<?php echo  theme_root . '/'; ?>includes/img/arrow-left-bottom.png" class="arrow hidden-xs">
+			     	   			<button type="button" class="btn btn-green">Request for Early Access</button>
+			     	        	   	
+			     	        	   </section>
 
 			     	   </div>
 
