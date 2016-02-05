1e042d62d7a90509e54ccf8d816a2fa74d20c2a9
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-05 23:26:26 +0500

    contact form  fix

---
+++ content-princing.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * 
+ *
+ * @package _tk
+ */
+?>
+
+        <!-- @include html_Moudles/header.html -->
+
+			           <div class="pricing">
+                               
+                          <h2>Simple and Affordable Price</h2>
+                               
+                               <div class="text-center social-icons-footer">
+                               <?php echo do_shortcode( '[social4i size="small"]' ); ?>
+                            </div>
+
+
+                          <div class="price_price m-b-20">
+                               <p>19.99 Per Month</p>
+                          </div>
+
+                         <p class="no-margin">Unlimited Project / 25 Users ( 1 $ / active user / month )</p>
+                          <p class="m-b-40">After a 60 days trial period.</p>
+
+                          <p>We also provide yearly plans, or for large companies:  <a href="">Contact Us</a></p>
+
+
+
+                      
+
+                        <section class="container-projoo cta-2">
+                          <h2 class="center-block m-b-30">Try Proojo – Maximum visibility with minimum clutter of tasks.</h2>
+                          <img src="http://www.proojo.com/wp-content/themes/projoo_theme/includes/img/arrow-left-bottom.png" class="arrow hidden-xs">
+                          <button type="button" class="btn btn-green" data-toggle="modal" data-target="#modal-form">Request for Early Access</button>
+                          
+                        </section>
+
+                          <div class="price_feature m-b-100">
+                               <h3 class="blue m-b-20">Every paid plan includes the following excellent features</h3>
+                               <ul class="list-unstyled">
+                                    <li>60 day free trial — no credit card needed</li>
+                                    <li>Unlimited archived projects</li>
+                                    <li>Discussions</li>
+                                    <li>24/7 Support Staff</li>
+                                    <li>File sharing</li>
+                                    <li>Time tracking</li>
+                                    <li>Wikis/Notebooks</li>
+                                    <li>No fixed contracts, cancel anytime</li>                         
+                               </ul>
+                          </div>
+
+
+
+
+                      </div>
+	
+     	  <?php get_template_part( 'includes/modal-form' ); ?>
+</div>
+</div>
+         <!-- @include html_Moudles/footer.html -->
+
