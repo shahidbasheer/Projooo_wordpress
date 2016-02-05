8ea17540ea7e93bdfa6f40a8f725741ac7fd883a
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-05 11:30:34 +0500

    vidio fox

---
+++ content-contact-us.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * 
+ *
+ * @package _tk
+ */
+?>
+
+        <!-- @include html_Moudles/header.html -->
+
+        <div class="seprater-bottom-70"></div>
+               
+                  <div class="contact-us">
+               
+
+                         
+                         <div class="intoro">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, illum.</p>
+                              <ul class="list-inline">
+                                   <li>example@example.com</li>
+                                   <li>+0978797498273498</li>
+                              </ul>
+                         </div>
+
+
+                         <div class="contact-us__address">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, illum.</p>
+                         </div>
+                        
+                           <div class="contact-us__form">
+                                <?php the_content(); ?>
+                           </div>
+                         
+                           
+
+                            <section class="container-projoo cta-2">
+                              <h2 class="center-block m-b-30">Try Proojo – Maximum visibility with minimum clutter of tasks.</h2>
+                              <img src="http://www.proojo.com/wp-content/themes/projoo_theme/includes/img/arrow-left-bottom.png" class="arrow hidden-xs">
+                              <button type="button" class="btn btn-green" data-toggle="modal" data-target="#modal-form">Request for Early Access</button>
+                              
+                            </section>
+
+                 </div>
+	
+     	  <?php get_template_part( 'includes/modal-form' ); ?>
+</div>
+</div>
+         <!-- @include html_Moudles/footer.html -->
+
---
+++ header-contact-us.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package _tk
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php do_action( 'before' ); ?>
+
+ <nav class="navbar navbar-custom" role="navigation">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	 <div class="container-projoo">
+					
+					<div class="navbar-header">
+					    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+					        <i class="fa fa-bars"></i>
+					    </button>
+					    <a class="navbar-brand page-scroll" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					        <img src="<?php echo  theme_root . '/'; ?>includes/img/logo.png">
+					    </a>
+					</div>
+
+					<!-- The WordPress Menu goes here -->
+					<?php wp_nav_menu(
+						array(
+							'theme_location' 	=> 'primary',
+							'depth'             => 2,
+							'container'         => 'div',
+							'container_class'   => 'collapse navbar-collapse navbar-right navbar-main-collapse',
+							'menu_class' 		=> 'nav navbar-nav',
+							'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
+							'menu_id'			=> 'main-menu',
+							'walker' 			=> new wp_bootstrap_navwalker()
+						)
+					); ?>			
+		
+	</div><!-- .container -->
+</nav><!-- .site-navigation -->
+
+<div class="headwrap">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+
+ <!-- <div class="seprater-bottom-70"></div> -->
+
+<div class="headwrap__blue about-us">
+	<div class="container-projoo">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h1 class="m-b-10">Contact Us</h1>		     	   					
+			</div>
+		</div>
+	</div>
+</div>
+
+     	  <div class="container-projoo blog">
+                 <div class="row">
+                      <div class="col-xs-12 <?php if ( is_home() ) { echo "col-sm-7 col-md-8 col-lg-8";  }  else {  echo "";  }   ?>">
+	
+		
