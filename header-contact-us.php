7c5a842ce851a84003d773727dcee2088a1e11be
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-07 03:29:38 +0500

    contact form  fix

--- header-contact-us.php
+++ header-contact-us.php
@@ -20,16 +20,19 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
-	    <!-- Hotjar Tracking Code for http://www.proojo.com -->
-	<script>
-	(function(h,o,t,j,a,r);
-	h._hjSettings=149892,hjsv;
-	a=o.getElementsByTagName('head')0;
-	r=o.createElement('script');r.async=1;
-	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-	a.appendChild(r);
-	})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
+	
+	<!-- Hotjar Tracking Code for http://www.proojo.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:149892,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
