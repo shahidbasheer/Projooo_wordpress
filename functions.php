6ff785cbce65508220db933887c3bb53743adfb4
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-06 22:07:38 +0500

    contact form  fix

--- functions.php
+++ functions.php
@@ -194,4 +194,8 @@ function wpdocs_excerpt_more( $more ) {
 add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 
 
-remove_filter ('the_content', 'wpautop');
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
