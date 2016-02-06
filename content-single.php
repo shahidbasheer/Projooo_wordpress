95f7bdf79b380dbe8ab85e61e6d4042c798c73fa
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-07 00:49:05 +0500

    contact form  fix

--- content-single.php
+++ content-single.php
@@ -8,7 +8,7 @@
                           <article class="single-post">
                                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="back-tp-home-page">Back to Blog Home Page</a>
                               <header>
-                                
+
                               	<?php the_title( sprintf( '<h2 class="page-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
                                     
                                     <div class="entry-meta">                                    
@@ -27,18 +27,22 @@
                                   <?php endif ?>
 
                                     <?php the_content(); ?>
+
+
                                    
                               </div>
 
-                              
+                                <div class="post-cta">
+                                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ab voluptatibus perspiciatis possimus, voluptates dolor eos error. Voluptas libero, repellendus!</p>
+                                     <button type="button" class="btn btn-green" data-toggle="modal" data-target="#modal-form">Request for Early Access</button>
+                                  </div>
+
+                              <?php echo do_shortcode( '[social4i size="small"]' ); ?>
                               <footer class="entry-meta">
           
                               </footer><!-- .entry-meta -->
 
-                               <div class="post-cta">
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ab voluptatibus perspiciatis possimus, voluptates dolor eos error. Voluptas libero, repellendus!</p>
-                                 <button type="button" class="btn btn-green" data-toggle="modal" data-target="#modal-form">Request for Early Access</button>
-                              </div>
+                               
 
                            </article>
                      
