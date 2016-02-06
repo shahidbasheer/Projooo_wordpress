62f3be3b6e675171cba9580be3f6ef189b8506f7
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-06 23:21:31 +0500

    contact form  fix

--- includes/list-categories-widget.php
+++ includes/list-categories-widget.php
@@ -40,7 +40,7 @@ class list_categories_widget extends WP_Widget {
 
 							
 								<?php foreach($cats as $cat) { ?>
-										<?php var_dump($cat); ?>
+										<?php //var_dump($cat); ?>
 										<?php  //var_dump(get_term_link($cat->slug, $taxonomy));  ?>
 										<?php $catlist = get_term_link($cat->slug, $taxonomy); ?>
 										
